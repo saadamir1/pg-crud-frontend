11f1584fb4f7f3ca586de6e1f215fbf39e1a3bdd
Saad Amir <Saadamir070@gmail.com>
2025-08-01 20:29:06 +0500

    temp: fix commits author

---
+++ fix-commits.bat
@@ -0,0 +1,3 @@
+@echo off
+set FILTER_BRANCH_SQUELCH_WARNING=1
+git filter-branch --env-filter "if [ \"$GIT_COMMITTER_EMAIL\" = \"samir@apac.microagility.com\" ]; then export GIT_COMMITTER_NAME=\"Saad amir\"; export GIT_COMMITTER_EMAIL=\"saadamir070@gmail.com\"; fi; if [ \"$GIT_AUTHOR_EMAIL\" = \"samir@apac.microagility.com\" ]; then export GIT_AUTHOR_NAME=\"Saad amir\"; export GIT_AUTHOR_EMAIL=\"saadamir070@gmail.com\"; fi" --tag-name-filter cat -- --branches --tags
